743c525e1171f1e62aa218fd9bfffc1b44a18c15
Michael Bruce <me@michaelbruce.co>
2019-02-04 18:15:50 +0000

    Attempted translation from js, does not produce SetMyLine function

--- src/ice/core.cljs
+++ src/ice/core.cljs
@@ -1,3 +1,13 @@
 (ns ice.core)
 
-(println "Hello world!")
+(defn onBufWrite [] (.log js/console "Buffer written!"))
+
+(set!
+  (.-exports js/module)
+  (fn [plugin]
+    (defn setLine [] (.setLine (.-nvim plugin) "A line from clojurescript, for your troubles"))
+    (.registerCommand
+      plugin
+      "SetMyLine"
+      #js [(.. plugin -nvim -buffer) setLine])
+    (.registerAutocmd plugin "BufWritePre" onBufWrite #js {:pattern "*"})))
