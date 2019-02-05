5b41ea03b995dfa30ae1634bd171e84a6e76714f
Michael Bruce <me@michaelbruce.co>
2019-02-05 00:11:31 +0000

    update build script, wrap plugin setup in a function

--- src/ice/core.cljs
+++ src/ice/core.cljs
@@ -1,13 +1,21 @@
 (ns ice.core)
 
+(println "HELLO?")
+(.log js/console "Buffer written!")
+
 (defn onBufWrite [] (.log js/console "Buffer written!"))
 
-(set!
-  (.-exports js/module)
-  (fn [plugin]
-    (defn setLine [] (.setLine (.-nvim plugin) "A line from clojurescript, for your troubles"))
-    (.registerCommand
-      plugin
-      "SetMyLine"
-      #js [(.. plugin -nvim -buffer) setLine])
-    (.registerAutocmd plugin "BufWritePre" onBufWrite #js {:pattern "*"})))
+(defn setup [& _]
+  (println "Starting setup method for ice")
+  (.log js/console "Buffer written!")
+  (set!
+    (.-exports js/module)
+    (fn [plugin]
+      (defn setLine [] (.setLine (.-nvim plugin) "A line from clojurescript, for your troubles"))
+      (.registerCommand
+        plugin
+        "SetMyLine"
+        #js [(.. plugin -nvim -buffer) setLine])
+      (.registerAutocmd plugin "BufWritePre" onBufWrite #js {:pattern "*"}))))
+
+(set! *main-cli-fn* setup)
