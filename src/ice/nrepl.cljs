9b18dede7c60c00afd3b7fc65579cf22e671ff6f
Michael Bruce <me@michaelbruce.co>
2019-02-05 07:57:21 +0000

    define contract for nrepl/connect method

--- src/ice/nrepl.cljs
+++ src/ice/nrepl.cljs
@@ -1,4 +1,6 @@
 (ns ice.nrepl)
 
-(defn connect [x]
-  "connected!")
+(defn connect
+  "returns a connection map, containing port and session id to communicate with a nREPL."
+  [port]
+  {:port port :session-id "x"})
--- test/ice/nrepl-test.cljs
+++ test/ice/nrepl-test.cljs
@@ -4,5 +4,8 @@
     [ice.nrepl :as nrepl]))
 
 (deftest connect
-  (testing "connects to nREPL on specified port"
-    (is (= (nrepl/connect "connected!")))))
+  ;; (testing "connects to nREPL on specified port"
+  ;;   (is (= (nrepl/connect "connected!"))))
+
+  (testing "returns a connection map"
+    (is (= {:port 9999 :session-id "x"} (nrepl/connect 9999)))))
