41170459148d7ee8b49271d806f0d644cd01b7d3
Michael Bruce <me@michaelbruce.co>
2019-02-06 08:56:01 +0000

    helpful tip on how to conj into the last element of a sequence.

--- test/ice/bencode-test.cljs
+++ test/ice/bencode-test.cljs
@@ -15,3 +15,6 @@
            {:new-session "2d2d78b1-24ab-43b6-8ef5-84e1d9e83367"
             :session "8fcca80c-7514-4b2b-82ff-f0915b90f753"
             :status "done"}))))
+
+;; TODO when implementing lists and inner dicts => try using pop to conj into
+;; inner vecs e.g (let [new-val (conj (peek my-vec) my-val)] (conj (pop my-vec) new-val))
